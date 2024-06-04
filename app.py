f5f2698774fcc88205eef2cfb336cd3bf40c42fd
Neevesh Jain <neeveshjain27@gmail.com>
2024-06-04 10:25:44 +0530

    changes

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_pymongo import PyMongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer
 
 app = Flask(__name__)
+app.config['MAIL_SERVER'] = 'smtp.your-email.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'your-email@example.com'
+app.config['MAIL_PASSWORD'] = 'your-email-password'
+app.config['MAIL_DEFAULT_SENDER'] = 'your-email@example.com'
+app.secret_key = 'your-secret-key'
 app.secret_key = "secret"
 app.config["MONGO_URI"] = "mongodb+srv://info:HR5HbNCrGWoJfUFs@authentication.489yabh.mongodb.net/Authentication?retryWrites=true&w=majority&appName=Authentication"
 mongo = PyMongo(app)
+
+mail = Mail(app)
+serializer = URLSafeTimedSerializer(app.secret_key)
 
 @app.route('/')
 def index():
@@ -48,6 +60,44 @@ def pricing():
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
+
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        user = mongo.db.users.find_one({'email': email})
+        
+        if user:
+            token = serializer.dumps(email, salt='password-reset-salt')
+            reset_url = url_for('reset_password', token=token, _external=True)
+            subject = "Password Reset Requested"
+            msg = Message(subject, recipients=[email])
+            msg.body = f"Please click the link to reset your password: {reset_url}"
+            mail.send(msg)
+            flash('Password reset email sent!', 'info')
+        else:
+            flash('Email not found!', 'danger')
+        
+        return redirect(url_for('login'))
+    
+    return render_template('forgot_password.html')
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    try:
+        email = serializer.loads(token, salt='password-reset-salt', max_age=3600)
+    except:
+        flash('The password reset link is invalid or has expired.', 'danger')
+        return redirect(url_for('forgot_password'))
+
+    if request.method == 'POST':
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        mongo.db.users.update_one({'email': email}, {'$set': {'password': hashed_password}})
+        flash('Your password has been updated!', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('reset_password.html')
 
 
 if __name__ == '__main__':
